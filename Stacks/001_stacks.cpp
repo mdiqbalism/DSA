1bac2f18173eb6b694d4ab370e9d4777bbf169eb
Md Iqbal Ali <mdiqbalali78692@gmail.com>
2023-12-21 23:48:25 +0530

    this is stack

---
+++ Stacks/001_stacks.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class stack{
+    //properties
+    public:
+    
+
+    //behavior
+
+};
+
+int main(){
+
+/*
+    //creation of stack
+    stack<int>s;
+
+    //push operation 
+    s.push(2);
+    s.push(3);
+
+    //pop 
+    s.pop();
+
+    cout << "Printing top element " << s.top() << endl;
+    cout << "Size of the stack is  " << s.size() << endl;
+
+    if (s.empty())
+    {
+       cout << "Stack is empty " << endl;
+    }
+    else
+    {
+        cout << "Stack is not empty " << endl;
+    }
+*/
+    
+    
+    return 0;
+}
