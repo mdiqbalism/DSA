d67b37abc7b4a0c1400331dabc26d085a5e1e19d
Md Iqbal Ali <mdiqbalali78692@gmail.com>
2023-12-23 18:18:15 +0530

    it is commited

--- Stacks/001_stacks.cpp
+++ Stacks/001_stacks.cpp
@@ -2,16 +2,99 @@
 #include<stack>
 using namespace std;
 
-class stack{
+class Stack{
     //properties
     public:
-    
+        int *arr;
+        int top;
+        int size;
 
     //behavior
+    Stack(int size){
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+           cout << "Stack Overflow" << endl;
+        }
+          
+    }
+
+    void pop(){
+        if (top >= 0)
+        {
+           top--;
+        }else
+        {
+           cout << "Stack Underflow " << endl;
+        }
+          
+    }
+
+    int peek(){
+        if (top >= 0 )
+        {
+           return arr[top];
+        }else
+        {
+            cout <<"Stack is Empty" << endl;
+            return -1;
+        }
+           
+    }
+
+    bool isEmpty(){
+        if (top == -1)
+        {
+           return true;
+        }else\
+
+        {
+           return false;
+        }
+           
+    }
 
 };
 
 int main(){
+    Stack st(5);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+
+    cout <<st.peek() << endl;
+
+    st.pop();
+    
+    cout<<st.peek()<< endl;
+
+    st.pop();
+    cout<<st.peek()<< endl;
+
+    st.pop();
+    cout<<st.peek()<< endl;
+
+    if (st.isEmpty())
+    {
+       cout <<"Stack is Empty " << endl;
+    }else{
+        cout << "Stack is not Empty " << endl;
+    }
+    st.pop();
+    
+    
+
+
 
 /*
     //creation of stack
@@ -22,7 +105,7 @@ int main(){
     s.push(3);
 
     //pop 
-    s.pop();
+    
 
     cout << "Printing top element " << s.top() << endl;
     cout << "Size of the stack is  " << s.size() << endl;
